3c66d753dec330a5183c03ed3e36f11cac7d9d2f
RuyiW <RuyiW@users.noreply.github.com>
2023-09-18 19:56:16 +0800

    added image page

---
+++ PDriveUI/Controllers/ImageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PDriveUI.Controllers
+{
+    public class ImageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PDriveUI/Models/Image.cs
@@ -0,0 +1,9 @@
+﻿namespace PDriveUI.Models
+{
+    public class Image
+    {
+        public string ID { get; set; }
+        public string ImageName { get; set; }
+        public string[] Tags { get; set; }
+    }
+}
